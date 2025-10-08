773d53baa9bd0fc4d00018f6e455860c59cf6e46
Rajat Tripathi <rajattripathi5967@gmail.com>
2025-10-08 20:56:18 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in );
         Library lib = new Library();
         lib.addBooks();
         boolean running = true;
